92300e9452962800170bc607a85d4ebc8fbd3a3e
zh970813653 <z15301502328@163.com>
2023-05-16 22:08:45 +0800

    minVue-router初始化

---
+++ src/myRouter/index.js
@@ -0,0 +1,33 @@
+let _Vue = null
+
+export default class VueRouter {
+    // install接收两个参数 第一个是vue的构造函数 第二个可选的选项对象
+    static install(Vue) {
+        // 1. 判断当前插件是否安装
+        if (VueRouter.install.installed) {
+            return 
+        }
+        VueRouter.install.installed = true
+        // 2. 把vue构造函数记录到全局变量中 （为什么记录？ 因为需要在实力方法中使用这个构造函数）
+        _Vue = Vue
+        // 3. 把创建Vue实例的时候传入的router对象注入到Vue实力上
+        // _Vue.prototype.$router new Vue  
+        _Vue.mixin({  
+            beforeCreate(){
+                if (this.$options.router) {
+                    _Vue.prototype.$router = this.$options.$router
+                }
+                
+            }
+        })
+    }
+    constructor(options) {
+        this.options = options
+        // 这个对象的对象的作用是把options中传入的routes解析出来存入  值存储的就是路由组件
+        this.routerMap = {}
+        // data是一个响应式对象， 里面有一个current记录的是当前路由的地址
+        this.data = _Vue.observable({
+            current: '/'
+        })
+    }
+}
